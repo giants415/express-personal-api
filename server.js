7c262af500b8dafaf618246b3098a912ec1e9a41
Andrew <andrew.vinocur@gmail.com>
2016-09-01 15:42:23 -0700

    updates API endpoints

--- server.js
+++ server.js
@@ -45,17 +45,18 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    woopsIForgotToDocumentAllMyEndpoints: false, // I have changed this
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/giants415/express-personal-api/blob/master/README.md", // I have changed this
+    baseUrl: "https://evening-tundra-69507.herokuapp.com/", // I have changed this
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me"}, // Sticking w/ this for the moment
+      {method: "POST", path: "/api/shows", description: "Where I will put the shows I like"} // I have changed this
     ]
   })
 });
+
 
 /**********
  * SERVER *

72cf64c3060ed1906308a9bc1a1f98e21a148e8e
Andrew <andrew.vinocur@gmail.com>
2016-09-02 12:00:06 -0700

    moves API endpoint documentation to a separate file in /documentations

--- server.js
+++ server.js
@@ -44,18 +44,8 @@ app.get('/', function homepage(req, res) {
 
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
-  res.json({
-    woopsIForgotToDocumentAllMyEndpoints: false, // I have changed this
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/giants415/express-personal-api/blob/master/README.md", // I have changed this
-    baseUrl: "https://evening-tundra-69507.herokuapp.com/", // I have changed this
-    endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // Sticking w/ this for the moment
-      {method: 'GET', path: '/api/shows', description: 'See all of the shows'},
-      {method: "POST", path: "/api/shows", description: "Where shows will be updated"} // I have changed this
-    ]
-  })
+  var documents = require('./views/documentations');
+  res.json(documents);
 });
 
 app.get('/api/profile', function(req, res) {
---
+++ views/documentations/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    woopsIForgotToDocumentAllMyEndpoints: false, // I have changed this
+    message: "Welcome to my personal api! Here's what you need to know!",
+    documentationUrl: "https://github.com/giants415/express-personal-api/blob/master/README.md", // I have changed this
+    baseUrl: "https://evening-tundra-69507.herokuapp.com/", // I have changed this
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes all available endpoints"},
+      {method: "GET", path: "/api/profile", description: "Data about me"}, // Sticking w/ this for the moment
+      {method: 'GET', path: '/api/shows', description: 'See all of the shows'},
+      {method: "POST", path: "/api/shows", description: "Where shows will be updated"} // I have changed this
+    ]
+  }
